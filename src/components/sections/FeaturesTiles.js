9e24bede0670b80ab2823a34075f4c1945001956
JAlbertCode <JonathanAlbert0115@gmail.com>
2022-08-27 19:52:29 -0400

    commented out two more lint warnings

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import { SectionTilesProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
-import Image from '../elements/Image';
+// import SectionHeader from './partials/SectionHeader';
+// import Image from '../elements/Image';
 
 const propTypes = {
   ...SectionTilesProps.types
